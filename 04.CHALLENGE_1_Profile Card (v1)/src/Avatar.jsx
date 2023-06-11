8a76c3a547437fe92297fa04714014b3f5aeef38
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-06-11 21:08:15 +0200

    upd

---
+++ 04.CHALLENGE_1_Profile Card (v1)/src/Avatar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import jonas from "./assets/jonas.jpeg";
+
+const Avatar = () => {
+  return <img src={jonas} alt="jonas" className="avatar" />;
+};
+
+export default Avatar;
---
+++ 04.CHALLENGE_1_Profile Card (v1)/src/Intro.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <div>
+        <h1>Jonas Schmedtmann</h1>
+        <p>
+          Full-stack web developer and teacher at Udemy. When not coding or
+          preparing a course, I like to play board games, to cook (and eat), or
+          to just enjoy the Portuguese sun at the beach.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
