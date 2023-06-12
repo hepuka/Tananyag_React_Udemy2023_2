01151dd4dd9304df6a9536fc7532fc8f61e5a273
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-06-12 13:58:59 +0200

    upd

--- 07.Travel list/src/components/Form.js
+++ 07.Travel list/src/components/Form.js
@@ -19,12 +19,12 @@ export default function Form({ onAddItems }) {
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
+      <h3>What do you need for your trip?</h3>
       <select
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
       >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
