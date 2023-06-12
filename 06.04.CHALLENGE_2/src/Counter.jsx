4c9e61a55180c069c96167026020d53550be4bc7
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-06-12 13:32:37 +0200

    upd

--- 06.04.CHALLENGE_2/src/Counter.jsx
+++ 06.04.CHALLENGE_2/src/Counter.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 const Counter = () => {
-  return <div>counter</div>;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <span>
+        {count === 0
+          ? `Today is `
+          : count > 0
+          ? `${count} days from today is `
+          : count < 0
+          ? `${Math.abs(count)} days ago was `
+          : ""}
+      </span>
+      <span>{date.toDateString()}</span>
+    </>
+  );
 };
 
 export default Counter;
