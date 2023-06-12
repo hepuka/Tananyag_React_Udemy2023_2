d0182de33ba134bd3825a204dedcaaee104339a4
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-06-12 09:26:28 +0200

    upd

---
+++ 05.Steps/src/App.js
@@ -0,0 +1,18 @@
+import Steps from "./Steps";
+import StepMessage from "./StepMessage";
+
+export default function App() {
+  return (
+    <div>
+      <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>😎</p>
+      </StepMessage>
+    </div>
+  );
+}
