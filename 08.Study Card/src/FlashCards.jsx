504c5bcda0a582a7cb59f9d33764d10a5230b148
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-06-12 15:38:31 +0200

    upd

---
+++ 08.Study Card/src/FlashCards.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { questions } from "./questions.js";
+import { useState } from "react";
+
+const FlashCards = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FlashCards;
