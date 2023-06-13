4a15ea7efcd1e3578f424e48d3d3e7b349e1ca86
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-06-13 14:38:10 +0200

    upd

---
+++ 12.Accordion Cards/src/Accordion.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AccordionItem from "./AccordionItem";
+
+const Accordion = ({ data }) => {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
