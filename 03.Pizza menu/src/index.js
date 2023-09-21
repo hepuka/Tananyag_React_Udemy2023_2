a5a3a3b6577ba2b4d84c25f2105db767ffaa5a04
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-09-21 14:43:11 +0200

    upd

--- 03.Pizza menu/src/index.js
+++ 03.Pizza menu/src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-// React before 18
-// ReactDOM.render(<App />, document.getElementById("root"));
