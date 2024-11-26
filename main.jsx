19816550e03c1383885a4949963c4ad09bec21da
hepuka <kunzoltan7707@gmail.com>
2024-11-26 22:18:12 +0100

    upd

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./src/11.UsePopcorn/App.jsx";
+import App from "./src/14.Currency converter/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
