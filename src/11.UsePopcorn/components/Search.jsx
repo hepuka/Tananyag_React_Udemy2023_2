9724ba67bac5d6e6766f9c2bd49d17fb26c7eb84
hepuka <kunzoltan7707@gmail.com>
2024-11-27 19:35:06 +0100

    upd

--- src/11.UsePopcorn/components/Search.jsx
+++ src/11.UsePopcorn/components/Search.jsx
@@ -1,6 +1,22 @@
-import { useState } from "react";
+import { useRef, useEffect } from "react";
 
 export const Search = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -8,6 +24,7 @@ export const Search = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
