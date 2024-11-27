f94e63f9e3fb5edf090ec73ce19e6c8df745aea7
hepuka <kunzoltan7707@gmail.com>
2024-11-27 20:53:24 +0100

    upd

--- src/11.UsePopcorn/components/Search.jsx
+++ src/11.UsePopcorn/components/Search.jsx
@@ -1,21 +1,14 @@
 import { useRef, useEffect } from "react";
+import { useKey } from "../useKey";
 
 export const Search = ({ query, setQuery }) => {
   const inputEl = useRef(null);
 
-  useEffect(() => {
-    function callback(e) {
-      if (document.activeElement === inputEl.current) return;
-
-      if (e.code === "Enter") {
-        inputEl.current.focus();
-        setQuery("");
-      }
-    }
-
-    document.addEventListener("keydown", callback);
-    return () => document.addEventListener("keydown", callback);
-  }, [setQuery]);
+  useKey("Enter", () => {
+    if (document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
 
   return (
     <input
