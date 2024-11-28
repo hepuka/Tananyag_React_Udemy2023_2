759d54a2bbf7f724add903f4f1a9cf90134320dd
hepuka <kunzoltan7707@gmail.com>
2024-11-28 18:00:35 +0100

    upd

--- src/16.React_quiz/App.jsx
+++ src/16.React_quiz/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import DateCounter from "./DateCounter";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 import "./App.css";
 
 const App = () => {
-  return <DateCounter />;
+  return (
+    <div className="app">
+      <Header />
+      <Main></Main>
+    </div>
+  );
 };
 
 export default App;
