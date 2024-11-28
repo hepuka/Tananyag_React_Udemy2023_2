65510770653bc56777d95d33932fe73432064e08
hepuka <kunzoltan7707@gmail.com>
2024-11-28 16:32:04 +0100

    upd

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./src/15.Geolocation/App.jsx";
+import App from "./src/16.React_quiz/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
---
+++ src/16.React_quiz/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DateCounter from "./DateCounter";
+import "./App.css";
+
+const App = () => {
+  return <DateCounter />;
+};
+
+export default App;
