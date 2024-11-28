a62553eb79565cde55e33e6eec402e5476f0b13f
hepuka <kunzoltan7707@gmail.com>
2024-11-28 18:15:58 +0100

    upd

--- src/16.React_quiz/App.jsx
+++ src/16.React_quiz/App.jsx
@@ -2,8 +2,34 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import "./App.css";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "Loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "Ready" };
+    case "dataFailed":
+      return { ...state, status: "Error" };
+    default:
+      throw new Error("Unknown action");
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
