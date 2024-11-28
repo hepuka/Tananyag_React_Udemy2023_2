c1fe0d6c321dd17a451c8baf39182fabbf409722
hepuka <kunzoltan7707@gmail.com>
2024-11-28 18:17:25 +0100

    upd

--- src/16.React_quiz/App.jsx
+++ src/16.React_quiz/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     fetch("http://localhost:3000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => console.log(err));
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
