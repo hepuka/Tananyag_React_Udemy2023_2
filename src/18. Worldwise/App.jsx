90dbf9f3359c92c649b4981f11b38f4668fef4a0
hepuka <kunzoltan7707@gmail.com>
2024-11-29 19:50:07 +0100

    upd

--- src/18. Worldwise/App.jsx
+++ src/18. Worldwise/App.jsx
@@ -11,13 +11,6 @@ import Form from "./components/Form";
 import SpinnerFullPage from "./components/SpinnerFullPage";
 import "./App.css";
 
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import Homepage from "./pages/Homepage";
-// import Login from "./pages/Login";
-// import AppLayout from "./pages/AppLayout";
-// import PageNotFound from "./pages/PageNotFound";
-
 const Homepage = lazy(() => import("./pages/Homepage"));
 const Product = lazy(() => import("./pages/Product"));
 const Pricing = lazy(() => import("./pages/Pricing"));
@@ -25,20 +18,20 @@ const Login = lazy(() => import("./pages/Login"));
 const AppLayout = lazy(() => import("./pages/AppLayout"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
-// dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB
-// dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB
-
-function App() {
+export function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
           <Suspense fallback={<SpinnerFullPage />}>
             <Routes>
+              {/* default route to localhost */}
               <Route index element={<Homepage />} />
               <Route path="product" element={<Product />} />
               <Route path="pricing" element={<Pricing />} />
               <Route path="login" element={<Login />} />
+
+              {/* nested routes to app route - child routes of app route */}
               <Route
                 path="app"
                 element={
@@ -47,12 +40,15 @@ function App() {
                   </ProtectedRoute>
                 }
               >
+                {/* index route is a default child route. This show up once the path is .../app*/}
                 <Route index element={<Navigate replace to="cities" />} />
                 <Route path="cities" element={<CityList />} />
-                <Route path="cities/:id" element={<City />} />
                 <Route path="countries" element={<CountryList />} />
+                <Route path="cities/:id" element={<City />} />
                 <Route path="form" element={<Form />} />
               </Route>
+              {/* end of nested routes */}
+
               <Route path="*" element={<PageNotFound />} />
             </Routes>
           </Suspense>
