0dc4e97b36ec16df147c6fb4a716223546479448
hepuka <kunzoltan7707@gmail.com>
2024-12-01 15:13:12 +0100

    upd

--- src/19.Atomic Blog/App.jsx
+++ src/19.Atomic Blog/App.jsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo } from "react";
 import { PostProvider } from "./PostContext";
 import "./App.css";
 import Header from "./components/Header.jsx";
@@ -6,26 +6,11 @@ import Posts from "./components/Posts.jsx";
 import FormAddPost from "./components/FormAddPost.jsx";
 import Archive from "./components/Archive.jsx";
 import Footer from "./components/Footer.jsx";
+import DarkButton from "./components/DarkButton.jsx";
 function App() {
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-  const [isFakeDark, setIsFakeDark] = useState(false);
-
-  useEffect(
-    function () {
-      document.documentElement.classList.toggle("fake-dark-mode");
-    },
-    [isFakeDark]
-  );
-
   return (
     <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
-
+      <DarkButton />
       <PostProvider>
         <Header />
         <Main />
---
+++ src/19.Atomic Blog/components/DarkButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useEffect, useState } from "react";
+const DarkButton = () => {
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  return (
+    <button
+      onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+      className="btn-fake-dark-mode"
+    >
+      {isFakeDark ? "☀️" : "🌙"}
+    </button>
+  );
+};
+
+export default DarkButton;
