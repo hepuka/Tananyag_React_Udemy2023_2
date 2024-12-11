9804b2132a3c2974a6687012bb43ea23c921decd
hepuka <kunzoltan7707@gmail.com>
2024-12-11 20:24:22 +0100

    upd

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./src/19.Atomic Blog/App.jsx";
+import App from "./src/20.Workout Timer/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
