3540910102b847ef1826f30bc6a787b7979f53bf
hepuka <kunzoltan7707@gmail.com>
2024-12-11 19:36:38 +0100

    upd

--- src/18. Worldwise/App.jsx
+++ src/18. Worldwise/App.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import "./App.css";
 
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import SpinnerFullPage from "./components/SpinnerFullPage";
-import "./App.css";
 
 const Homepage = lazy(() => import("./pages/Homepage"));
 const Product = lazy(() => import("./pages/Product"));
