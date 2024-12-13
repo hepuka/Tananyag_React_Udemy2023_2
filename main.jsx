3516354ff924bb0f11ba7d3dae8df6d8df471db8
hepuka <kunzoltan7707@gmail.com>
2024-12-13 22:04:31 +0100

    upd

--- main.jsx
+++ main.jsx
@@ -2,13 +2,13 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./src/21.BankAccount-Redux/App.jsx";
-import store from "./src/21.BankAccount-Redux/store.jsx";
+import App from "./src/22.Fast_React_Pizza/App.jsx";
+import { Provider } from 'react-redux';
+import store from './src/22.Fast_React_Pizza/store.js';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
+       <Provider store={store}>
       <App />
     </Provider>
   </StrictMode>
