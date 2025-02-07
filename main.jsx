7bd76a4cc71f07ddd5d9a9148051087124694d56
kun-fagyal_zoltan <kunzoltan7707@gmail.com>
2025-02-07 19:59:55 +0100

    upd

--- main.jsx
+++ main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import store from './src/21.BankAccount-Redux/store';
-import App from './src/07.CHALLENGE_2_Date Counter/App';
+import App from './src/04.Pizza menu/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/04.Pizza menu/Pizza.jsx
+++ src/04.Pizza menu/Pizza.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Pizza = ({ pizzaObj }) => {
+  if (pizzaObj.soldOut) return null;
+
   return (
-    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
